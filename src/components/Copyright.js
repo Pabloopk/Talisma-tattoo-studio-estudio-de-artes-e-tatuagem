ae76a96ab84ac7b313e8978a21ef46c3722f5851
Pablo <parkour.pablo458@gmail.com>
2025-03-10 22:15:59 -0300

    year

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -17,7 +17,7 @@ const Copyright = () => {
         }}
         viewport={{ once: false, amount: 0}}
         className='tracking-[0.02em] text-base'>
-          &copy; 2024 <span className='font-semibold'>Talismã Tattoo Studio.</span> || Todos os direitos reservados. || Desenvolvido por <span className='font-semibold hover:text-blue-900'><a href='https://wa.me/5561992295015'>Pablo Nunes.</a></span>
+          &copy; {new Date().getFullYear()} <span className='font-semibold'>Talismã Tattoo Studio.</span> || Todos os direitos reservados. || Desenvolvido por <span className='font-semibold hover:text-blue-900'><a href='https://wa.me/5561992295015'>Pablo Nunes.</a></span>
         </motion.div>
         
       </div>
